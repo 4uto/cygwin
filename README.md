5f093ab5f3c8d7e9a1d977f6a2e5689602792308
Sebastian Vilstrup <sebastian.vilstrup@gmail.com>
2016-01-09 18:42:48 +0100

    first release

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# cygwin
 Scripts and helpers to make life with cygwin easier
+
+Run 
+
+    install-cygwin.bat
+	
+To do a no-hands-required install of cygwin.
+It will download the install file automatically and then install my default choices of tools, without any interaction required.
+
+Currently it installs these tools, in addition to the defaults:
+
+    curl,wget,xz,nano,ncurses,openssh,patch,readline,unzip,chere,lftp,gnupg,rsync
+	
+To get explorer integration, just double-click the 
+
+    cygwinExplorerIntegration.reg
---
+++ install-cygwin.bat
@@ -0,0 +1,14 @@
+@echo off
+
+if not exist setup-x86.exe (
+  echo *** Fetching cygwin setup file: setup-x86.exe
+  powershell -Command "(new-object System.Net.WebClient).DownloadFile('https://cygwin.com/setup-x86.exe','setup-x86.exe')"
+)
+
+setup-x86.exe -q -a x86 -R c:\cygwin -l c:\cygwin\packages -s http://cygwin.mbwarez.dk -P "curl,wget,xz,nano,ncurses,openssh,patch,readline,unzip,chere,lftp,gnupg,rsync"
+
+echo *** Installation is complete. To get explorer integration, open the Cygwin Terminal as Administrator and run "chere -i -t mintty"
+
+rem chere -i -t mintty
+
+pause
